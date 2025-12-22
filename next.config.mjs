a7f82973d0fdbbc0b027e6549691f7bd727ecb39
Pixel0001 <racustefan34@gmail.com>
2025-12-22 20:30:07 +0200

    fix: clean image config for optimization

--- next.config.mjs
+++ next.config.mjs
@@ -3,27 +3,18 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "rrxo4xu1zm7luffe.public.blob.vercel-storage.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "**.public.blob.vercel-storage.com",
-        pathname: "/**",
       },
     ],
   },
